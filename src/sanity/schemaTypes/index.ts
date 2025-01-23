0ce462d90ed88bc42b2a65f9c8fc32f55c95cce0
Amuzammill <Amuzammill>
2025-01-23 18:18:15 +0500

    Normalize line endings across the project

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -5,7 +5,7 @@ import { categoryType } from "./categoryType";
 import { productType } from "./productType";
 import { customerType } from "./customerType";
 import { orderType } from "./orderType";
-import { orderItemType } from "./orderItemType";
+
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [blockContentType, categoryType, productType, customerType, orderType],
